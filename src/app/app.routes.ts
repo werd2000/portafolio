923d07fa311cf0c19a27599de0557486c2744d7b
Walter Enrique Ruiz Diaz <walter.enrique.ruiz.diaz@gmail.com>
2017-09-02 09:24:59 -0300

    Agregamos modificaciones al menú

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,10 +7,10 @@ import {
 } from "./components/index.paginas";
 
 const APP_ROUTES: Routes = [
-  { path: '', component: PortafolioComponent },
+  { path: 'home', component: PortafolioComponent },
   { path: 'about', component: AboutComponent },
   { path: 'portafolio-item', component: PortafolioItemComponent },
-  { path: '**', pathMatch: 'full', redirectTo: '' }
+  { path: '**', pathMatch: 'full', redirectTo: 'home' }
 ];
 
 export const APP_ROUTING = RouterModule.forRoot(APP_ROUTES, { useHash: true });
