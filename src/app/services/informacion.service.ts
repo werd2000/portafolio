42f1a93737a590fa7fab3483db41688352eba81d
Walter Enrique Ruiz Diaz <walter.enrique.ruiz.diaz@gmail.com>
2017-09-02 21:21:05 -0300

    Servicio firebase listo

--- src/app/services/informacion.service.ts
+++ src/app/services/informacion.service.ts
@@ -6,16 +6,28 @@ export class InformacionService {
 
   info:any = {};
   cargada:boolean = false;
+  cargada_sobre_nosotros:boolean = false;
+  equipo:any[] = [];
 
   constructor( public http:Http ) {
+    this.carga_info();
+    this.carga_sobre_nosotros();
+  }
 
+  public carga_info(){
     this.http.get("assets/data/info.pagina.json")
               .subscribe(data => {
-                console.log(data.json());
                 this.cargada = true;
                 this.info = data.json();
               })
+  }
 
+  public carga_sobre_nosotros(){
+    this.http.get("https://templateangular.firebaseio.com/equipo.json")
+              .subscribe(data => {
+                this.cargada_sobre_nosotros = true;
+                this.equipo = data.json();
+              })
   }
 
 }
